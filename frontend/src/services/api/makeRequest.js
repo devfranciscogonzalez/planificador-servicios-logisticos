b63206593aeda60f8abcaea3eff2b00317d1571e
Francisco González <devfranciscogonzalez@gmail.com>
2024-08-11 01:01:28 -0400

    Refactor makeRequest.js for improved error handling and header management

--- frontend/src/services/api/makeRequest.js
+++ frontend/src/services/api/makeRequest.js
@@ -1,39 +1,54 @@
 import httpClient from "./httpClient";
+
 const DEFAULT_HEADERS = {
   "Content-Type": "application/json",
   Accept: "application/json",
 };
 
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function getHeaders(data) {
+  const headers = { ...DEFAULT_HEADERS };
+  headers['X-XSRF-TOKEN'] = getCookie('XSRF-TOKEN');
+  return data instanceof FormData ? {} : headers;
+}
+
+function handleError(error) {
+  if (error.response) {
+    return error.response.data;
+  } else if (error.request) {
+    return "No se recibió respuesta del servidor";
+  }
+  return "Error al realizar la solicitud";
+}
+
 async function makeRequest(method, url, data = null) {
   try {
     const config = {
-      method: method,
-      url: url,
-      headers: data instanceof FormData ? {} : DEFAULT_HEADERS,
-      data: data,
+      method,
+      url,
+      headers: getHeaders(data),
+      data,
     };
 
     const response = await httpClient(config);
     return response.data;
   } catch (error) {
-    let errorMessage;
+    const errorMessage = handleError(error);
 
-    if (error.response) {
-      errorMessage = error.response.data;
-    } else if (error.request) {
-      errorMessage = "No se recibió respuesta del servidor";
+    if (errorMessage && typeof errorMessage === 'object') {
+      errorMessage.errors && console.error("Errors:", errorMessage.errors);
+      errorMessage.message && console.error("Message:", errorMessage.message);
     } else {
-      errorMessage = "Error al realizar la solicitud";
-    }
-
-    if (errorMessage !== null) {
-      errorMessage?.errors && console.log("errors: " + errorMessage?.errors);
-      errorMessage?.message && console.log("mesaje: " + errorMessage?.message);
-    } else {
-      console.log(error);
+      console.error("Error:", errorMessage);
     }
 
     throw errorMessage;
   }
 }
+
 export default makeRequest;
