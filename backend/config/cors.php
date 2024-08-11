8491117db78ff2a1fd1e24d7f7f4b2271737cbee
Francisco González <devfranciscogonzalez@gmail.com>
2024-08-11 15:58:46 -0400

    Update cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins'  => explode(',', env('CORS_ALLOWED_ORIGINS')),
+    'allowed_origins'  => ['https://planificador-servicios-logisticos.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
