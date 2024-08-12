2fbacb75a5b218f031f454ede3699d58084926a7
Francisco González <devfranciscogonzalez@gmail.com>
2024-08-11 22:12:34 -0400

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3000,
+  },
   server: {
     // host: true,
     port: 3000,
