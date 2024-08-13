36075c7ec081d403a1dc6044115e868644379f29
Francisco González <devfranciscogonzalez@gmail.com>
2024-08-12 21:08:54 -0400

    .

--- frontend/src/routes/ProtectedRoutes.jsx
+++ frontend/src/routes/ProtectedRoutes.jsx
@@ -1,9 +1,13 @@
-import { Navigate, Outlet } from "react-router-dom";
+import {
+  // Navigate,
+  Outlet,
+} from "react-router-dom";
 import useAuth from "../features/auth/hooks/useAuth";
 
 const ProtectedRoutes = () => {
   const { isAuthenticated } = useAuth();
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+  return isAuthenticated ? <Outlet /> : <Outlet />;
+  // <Navigate to="/" />
 };
 
 export default ProtectedRoutes;
