e8a5b69571d34603303ede3758422a27b4bb1f18
Francisco González <devfranciscogonzalez@gmail.com>
2024-08-21 20:11:34 -0400

    Update user role permissions and descriptions

--- frontend/src/pages/HomeaPage/HomeaPage.jsx
+++ frontend/src/pages/HomeaPage/HomeaPage.jsx
@@ -65,10 +65,10 @@ const HomePage = () => {
                 Roles y funcionalidades:
               </Typography>
               <Typography variant="body1" gutterBottom>
-                ¿Alguna vez te has preguntado qué funciones pueden realizar cada
-                miembro del equipo? A continuación, se detallan las
-                responsabilidades y funciones específicas asignadas a cada rol
-                dentro de la organización.
+                ¿Alguna vez te has preguntado qué funciones realiza cada miembro
+                del equipo? A continuación, se detallan las responsabilidades y
+                funciones específicas asignadas a cada rol dentro de la
+                organización.
               </Typography>
 
               {roles?.map((role) => (
@@ -126,42 +126,42 @@ const HomePage = () => {
                 <FlujoItem
                   icon={<Filter1Icon sx={{ color: ROLE_COLORS[1] }} />}
                   primary="Creación Tarifa (Jefe Comercial)"
-                  secondary="La creación de una nueva tarifa se realiza en el apartado Tarifas / Nueva Tarifa..."
+                  secondary="La creación de una nueva tarifa se realiza en el apartado Tarifas / Nueva Tarifa. Al completar el formulario de creación, se selecciona el cliente correspondiente. Luego, se agrega el tipo de servicio, lo que permite seleccionar los servicios vinculados al tipo de servicio elegido. Posteriormente, se indica el producto, el cual automáticamente muestra el tipo de negocio asociado. A continuación, se selecciona la ruta, escogiendo entre las ya registradas. Una vez completado el formulario, se verifica que todo esté correcto y se procede a la creación de la tarifa, especificando la fecha de inicio y fin, junto con el precio. Finalmente, se realiza una última revisión para asegurarse de que todo esté en orden."
                 />
                 <FlujoItem
                   icon={<Filter2Icon sx={{ color: ROLE_COLORS[2] }} />}
                   primary="Planificación de Servicios (Customer Service)"
-                  secondary="Con una tarifa vigente, se puede acceder al apartado Orden de Servicio (OS) / Nueva Orden de Servicio..."
+                  secondary="Con una tarifa vigente, se puede acceder al apartado Orden de Servicio (OS) / Nueva Orden de Servicio. Allí se encuentra el formulario de creación de la OS. Se ingresa un código de tarifa válido, lo que permite acceder al formulario para crear la OS, indicando la fecha, planificación y horario. Los demás datos necesarios se extraen de la tarifa vigente asociada al código. Después de una revisión final, si todo es correcto, se acepta y se crea la OS."
                 />
                 <FlujoItem
                   icon={<Filter3Icon sx={{ color: ROLE_COLORS[2] }} />}
                   primary="Ingreso de Patente (Customer Service)"
-                  secondary="Cuando se recibe la información del camión asignado para cumplir la OS, se ingresa la patente..."
+                  secondary="Cuando se recibe la información del camión asignado para cumplir la OS, se ingresa la patente en el apartado Ingresar Patente. En este formulario, se completa el campo de la patente del camión en el formato XX-XX-XX."
                 />
                 <FlujoItem
                   icon={<Filter4Icon sx={{ color: ROLE_COLORS[3] }} />}
                   primary="Registrar Ingreso (Portero)"
-                  secondary="En el apartado OS Ingreso, se registra el ingreso del camión a las instalaciones..."
+                  secondary="En el apartado OS Ingreso, se registra el ingreso del camión a las instalaciones. Es importante destacar que solo se pueden registrar las OS del día y aquellas que tienen una patente de camión asignada. El portero deberá verificar que la información coincida con el camión que llega."
                 />
                 <FlujoItem
                   icon={<Filter5Icon sx={{ color: ROLE_COLORS[4] }} />}
                   primary="Romaneo de Ingreso (Romana)"
-                  secondary="El camión que ingresa a las instalaciones debe pasar por la romana, donde se registra el peso..."
+                  secondary="El camión que ingresa a las instalaciones debe pasar por la romana, donde se registra el peso en el apartado OS Ingreso Peso. En este apartado, se pueden visualizar los camiones que han ingresado a las instalaciones."
                 />
                 <FlujoItem
                   icon={<Filter6Icon sx={{ color: ROLE_COLORS[5] }} />}
                   primary="Confirmar/Rechazar Orden de Servicio (Supervisor)"
-                  secondary="Una vez que el camión ha ingresado y ha sido pesado, el supervisor confirma la OS..."
+                  secondary="Una vez que el camión ha ingresado y ha sido pesado, el supervisor confirma la OS en el apartado OS Confirmación . Si todo está correcto, se puede confirmar; de lo contrario, se rechaza. Opcionalmente, se pueden agregar comentarios y, si es necesario, se indica el número de contenedor."
                 />
                 <FlujoItem
                   icon={<Filter7Icon sx={{ color: ROLE_COLORS[4] }} />}
                   primary="Romaneo de Salida (Romana)"
-                  secondary="El camión que sale de las instalaciones debe pasar nuevamente por la romana..."
+                  secondary="El camión que sale de las instalaciones debe pasar nuevamente por la romana, donde se registra el peso en el apartado OS Salida Peso. Aquí se pueden visualizar los camiones que están saliendo de las instalaciones, que ya ingresaron, fueron pesados y cuya OS fue aceptada o rechazada por el supervisor."
                 />
                 <FlujoItem
                   icon={<Filter8Icon sx={{ color: ROLE_COLORS[3] }} />}
                   primary="Salida de Transportista (Portero)"
-                  secondary="En el apartado OS Salida, se registra la salida del camión de las instalaciones..."
+                  secondary="El camión que sale de las instalaciones debe pasar nuevamente por la romana, donde se registra el peso en el apartado OS Salida Peso. Aquí se pueden visualizar los camiones que están saliendo de las instalaciones, que ya ingresaron, fueron pesados y cuya OS fue aceptada o rechazada por el supervisor."
                 />
               </List>
 
